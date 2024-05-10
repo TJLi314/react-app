12161c94d8838ad289bc90f060579628f8eaef89
tianjili <tianjili@tesla.com>
2024-05-10 16:02:46 -0500

    Alert message on button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
 import Alert from "./components/Alert"
+import Button from "./components/Button";
 
 function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
+
   return (
     <div>
-      <Alert>
-        Hello <span>World</span>
-      </Alert>
+      {alertVisible && <Alert onClose={() => setAlertVisibility(false)}>LIVERPOOL</Alert>}
+      <Button onClick={() => setAlertVisibility(true)} type="primary">
+        Liverpool
+      </Button>
     </div>
   );
 }
