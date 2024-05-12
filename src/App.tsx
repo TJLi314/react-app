2f226e35b5d85c952f07cc756041769c4ab2e0cc
TJ Li <TianjianLi314@gmail.com>
2024-05-12 13:56:14 -0500

    Expense Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,40 @@
 import { useState } from "react";
-import Alert from "./components/Alert";
-import Button from "./components/Button";
-import Like from "./components/Like";
-import produce from "immer";
-import NavBar from "./components/NavBar";
-import Cart from "./components/Cart";
-import ExpandableText from "./components/ExpandableText";
-import Form from "./components/Form";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
+import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
+import ExpenseForm from "./expense-tracker/components/ExpenseForm";
+import categories from "./expense-tracker/Categories";
 
 function App() {
-  // const [alertVisible, setAlertVisibility] = useState(false);
-  // return (
-  //   <div>
-  //     {alertVisible && <Alert onClose={() => setAlertVisibility(false)}>LIVERPOOL</Alert>}
-  //     <Button onClick={() => setAlertVisibility(true)} type="primary">
-  //       Liverpool
-  //     </Button>
-  //   </div>
-  // );
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 10, category: "Groceries" },
+    { id: 2, description: "bbb", amount: 10, category: "Utilities" },
+    { id: 3, description: "ccc", amount: 10, category: "Entertainment" },
+    { id: 4, description: "ddd", amount: 10, category: "Utilities" },
+  ]);
 
-  // const cities = ["New York", "Los Angeles", "San Francisco"];
-  // return (
-  //   <ListGroup
-  //     items={cities}
-  //     heading="Miami"
-  //     onSelectItem={(item: string) => console.log(item)}
-  //   />
-  // );
-
-  // return <Like onClick={() => console.log("Clicked")} />;
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((e) => e.category === selectedCategory)
+    : expenses;
 
   return (
     <div>
-      <Form />
+      <div className="mb-5">
+        <ExpenseForm
+          onSubmit={(expense) =>
+            setExpenses([...expenses, { ...expense, id: expenses.length + 1 }])
+          }
+        />
+      </div>
+      <div className="mb-3">
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        />
+      </div>
+      <ExpenseList
+        expenses={visibleExpenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id != id))}
+      />
     </div>
   );
 }
